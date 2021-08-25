fe060c5ba5cd8b8558076748be79e4bd37e13a76
Clendon DuRapau <clendon.durapau@gmail.com>
2021-08-25 02:32:38 -0500

    swapped bodyparser with express.json()

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const db = require('../database');
 
 const app = express();
 const PORT = 3000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.use(express.static(__dirname + '/../client/dist'));
 
